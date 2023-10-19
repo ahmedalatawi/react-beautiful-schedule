bbd732aa1b0aab9fc238d08ee7f9da1e14957e69
Ahmed Alatawi <aalat001@gmail.com>
2023-10-19 01:21:47 -0400

    Added demo title and link to source code

--- demo/index.tsx
+++ demo/index.tsx
@@ -5,12 +5,46 @@ import './index.css'
 
 import WeeklyScheduleDemo from './WeeklyScheduleDemo'
 import eventsData from './eventsData'
+import styled from 'styled-components'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 function App() {
-  return <WeeklyScheduleDemo showDateNavigator showFooter className='demo-schedule' events={eventsData} />
+  return (
+    <Container>
+      <TitleContainer>
+        <Title>Live demo</Title>
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://github.com/ahmedalatawi/react-beautiful-schedule/tree/main/demo'
+        >
+          demo source code
+        </a>
+      </TitleContainer>
+      <WeeklyScheduleDemo showDateNavigator showFooter className='demo-schedule' events={eventsData} />
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`
+
+const Title = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+`
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+  flex-direction: column;
+`
 
 root.render(
   <React.StrictMode>
