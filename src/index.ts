12a7b83fc985c3294ee1f3fa84788cc067c2dd6d
Ahmed Alatawi <aalat001@gmail.com>
2023-10-24 02:34:16 -0400

    Exported ScheduleProps, ScheduleEventProps as type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import DateNavigator from './components/DateNavigator/DateNavigator'
-import { ScheduleEventProps } from './components/Event/EventCard'
-import { ScheduleProps } from './components/Schedule/Schedule.types'
 import WeeklySchedule from './components/Schedule/WeeklySchedule'
+
+import type { ScheduleEventProps } from './components/Event/EventCard'
+import type { ScheduleProps } from './components/Schedule/Schedule.types'
+
 import { backgrounds as backgroundColors, colors, themes as eventThemes } from './styles'
 
-export { WeeklySchedule, DateNavigator, ScheduleProps, ScheduleEventProps, eventThemes, colors, backgroundColors }
+export { WeeklySchedule, DateNavigator, eventThemes, colors, backgroundColors }
+export type { ScheduleProps, ScheduleEventProps }
