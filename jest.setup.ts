154df0c0cf5f90813714a3deff406447bc829d0e
Ahmed Alatawi <aalat001@gmail.com>
2023-11-05 13:18:11 -0500

    WIP

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,15 +1,1 @@
 import '@testing-library/jest-dom'
-import preset from 'ts-jest/presets/index.js'
-
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  ...preset.defaultsESM,
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-      },
-    ],
-  },
-}
